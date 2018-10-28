d0d0ccac843bfbf0fb17f2af11a746680d54276b
kulico <42776014+kulico@users.noreply.github.com>
2018-10-28 22:25:44 +0700

    please work3

--- control_script2.sh
+++ control_script2.sh
@@ -1,2 +1,27 @@
 sh /u01/app/oraInventory/orainstRoot.sh
 sh /u01/app/oracle/product/18.0.0/dbhome_1/root.sh
+
+echo "#
+
+
+
+# This file is used by ORACLE utilities.  It is created by root.sh
+# and updated by either Database Configuration Assistant while creating
+# a database or ASM Configuration Assistant while creating ASM instance.
+
+# A colon, ':', is used as the field terminator.  A new line terminates
+# the entry.  Lines beginning with a pound sign, '#', are comments.
+#
+# Entries are of the form:
+#   $ORACLE_SID:$ORACLE_HOME:<N|Y>:
+#
+# The first and second fields are the system identifier and home
+# directory of the database respectively.  The third field indicates
+# to the dbstart utility that the database should , "Y", or should not,
+# "N", be brought up at system boot time.
+#
+# Multiple entries with the same $ORACLE_SID are not allowed.
+#
+#
+jellybear18c:/u01/app/oracle/product/18.0.0/dbhome_1:Y" >/etc/oratab
+
