e77807df58cbd2cb58afa98310bc791b3024ca10
kulico <42776014+kulico@users.noreply.github.com>
2018-10-29 21:28:10 +0700

    please work7

--- control_script2.sh
+++ control_script2.sh
@@ -25,3 +25,10 @@ echo "#
 #
 jellybear18c:/u01/app/oracle/product/18.0.0/dbhome_1:Y" >/etc/oratab
 
+
+mv /home/git_scripts/dbora.sh /etc/init.d/
+chmod 750 /etc/init.d/dbora.sh
+chown root:dba /etc/init.d/dbora.sh
+chkconfig --add dbora.sh
+chkconfig dbora.sh on
+
